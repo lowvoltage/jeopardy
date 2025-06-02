81b7d6a53887cc8500614a77e22fec87fc16c0bd
Dimitar Kovachev <d.kovachev@gmail.com>
2025-06-02 19:16:25 +0300

    quick and dirty test

---
+++ jeopardy/tests/integration_test.py
@@ -0,0 +1,17 @@
+import os
+from tempfile import TemporaryDirectory
+
+from fastapi.testclient import TestClient
+
+def test_read_main() -> None:
+    with TemporaryDirectory() as d:
+        os.environ['J_DATA_PATH'] = d
+        from server_main import app
+
+        client = TestClient(app)
+        # This is going to be SLOW
+        # TODO: Mock the CSV and or DB
+        response = client.get('/question/?round=Jeopardy!&value=$200')
+        assert response.status_code == 200
+        assert response.json()['round'] == 'Jeopardy!'
+        assert response.json()['value'] == '$200'
